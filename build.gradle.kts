8e0152c0dfdf6850f489a4c69ff952af268bdc64
Giuseppe Barbieri <elect86@gmail.com>
2024-06-25 09:35:19 +0200

    showcase ready

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
-    `java-library`
-    kotlin("jvm") version "1.5.10"
-    application
-    id("elect86.jextract") //version "0.1.6"
-//    id("io.github.krakowski.jextract") version "0.1.6"
-    idea
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    embeddedKotlin("jvm")
 }
 
 group = "org.example"
@@ -16,71 +7,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.scijava.org/content/groups/public")
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-//    implementation(files("opengl.jar"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
+    implementation(platform("org.scijava:pom-scijava:38.0.0-SNAPSHOT"))
 
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-}
+    annotationProcessor(libs.scijava.common)
 
-application {
-    mainClass.set("MainKt")
-    mainClass.set("Teapot")
-    applicationDefaultJvmArgs += "-Djava.library.path=.:/usr/lib/x86_64-linux-gnu"
-}
+    implementation(kotlin("reflect"))
+    implementation(libs.org.jetbrains.kotlinx.kotlinxCoroutinesCore)
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-
-tasks.withType<JavaExec>().configureEach {
-    javaLauncher.set(javaToolchains.launcherFor(java.toolchain))
-}
-
-tasks {
-
-    withType<JavaCompile>().configureEach {
-        options.isIncremental = false
-    }
-
-    jextract {
-
-        jdk {
-
-        }
-
-
-
-        header("/usr/include/GL/glut.h") {
-            // The library name
-            libraries.addAll("glut", "GLU", "GL")
-
-            // The package under which all source files will be generated
-            targetPackage.set("opengl")
-
-            includes.add("/usr/lib/x86_64-linux-gnu")
-
-            sourceMode.set(true)
-            // The generated class name
-//            className = 'Linux'
-        }
-    }
-
-    register<ShadowJar>("testJar") {
-        archiveClassifier.set("tests")
-        from(sourceSets.test.get().output)
-        configurations.add(project.configurations.testImplementation.get())
-    }
-}
-//println(project.properties["org.gradle.java.installations.paths"])
-
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        verbose = true
-//        freeCompilerArgs = listOf("--add-modules", "jdk.incubator.foreign")
-    }
+    implementation(libs.org.slf4j.slf4jApi)
+    implementation(libs.org.joml.joml)
+    implementation(libs.org.jocl.jocl)
+    implementation(libs.scijava.common)
+    implementation(libs.scijava.scriptEditor)
+    implementation(libs.scijava.uiBehaviour)
+    implementation(libs.scijava.scriptingJython)
+    implementation(libs.net.java.dev.jna.jnaPlatform)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,15 @@
 rootProject.name = "test"
 
-includeBuild("../jextract")
-//includeBuild("../gradle-jextract")
+
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://maven.scijava.org/content/groups/public")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("org.scijava:pom-scijava:38.0.0-SNAPSHOT")
+        }
+    }
+}
+
